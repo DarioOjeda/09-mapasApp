01c267be7beb3cdf18ca89845fa6a41a8858694d
DarioOjeda <darioojeda_1995@hotmail.com>
2021-08-05 14:44:49 +0200

    listener para detectar cambios en el mapa y obtener latitud y longitud

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -26,6 +26,7 @@ export class ZoomRangeComponent implements OnInit, AfterViewInit {
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 10;
+  center: [number, number] = [-5.322671059976026, 36.34116260979489];
 
   constructor() { 
     // console.log( 'Constructor',this.divMapa);
@@ -43,7 +44,7 @@ export class ZoomRangeComponent implements OnInit, AfterViewInit {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
-      center: [-5.322671059976026, 36.34116260979489],
+      center: this.center,
       zoom: this.zoomLevel
       });
 
@@ -55,6 +56,14 @@ export class ZoomRangeComponent implements OnInit, AfterViewInit {
         if ( this.mapa.getZoom() > 18){
           this.mapa.zoomTo( 18 );
         }
+      });
+
+      //moviemineto del mapa
+      this.mapa.on('move', (event) => {
+        const target = event.target;
+        const { lng, lat} = target.getCenter();
+        
+        this.center = [lng, lat];
       });
   }
 
